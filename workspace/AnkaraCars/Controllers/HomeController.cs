9549d49d337b838205b472cbc78d40d6f2867330
Burak ARAS <31282950+arasbey@users.noreply.github.com>
2018-02-07 21:38:25 +0200

    initial project
    initial

---
+++ workspace/AnkaraCars/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AnkaraCars.Models;
+
+namespace AnkaraCars.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+        
+        public IActionResult Hizmetler()
+        {
+            return View();
+        }
+
+        public IActionResult Iletisim()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
